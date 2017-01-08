82eb6b58c169e9d725662d1176205db0162f79c2
Hiroki Noda <kubo39@gmail.com>
2017-01-09 06:01:40 +0900

    Compute 8 value at once with bigger precompiled table.

--- src/crc32.rs
+++ src/crc32.rs
@@ -1,17 +1,18 @@
 use std::hash::Hasher;
+use std::mem;
 
 pub const CASTAGNOLI: u32 = 0x82f63b78;
 pub const IEEE: u32 = 0xedb88320;
 pub const KOOPMAN: u32 = 0xeb31d82e;
 
 lazy_static! {
-    pub static ref IEEE_TABLE: [u32; 256] = make_table(IEEE);
-    pub static ref CASTAGNOLI_TABLE: [u32; 256] = make_table(CASTAGNOLI);
-    pub static ref KOOPMAN_TABLE: [u32; 256] = make_table(KOOPMAN);
+    pub static ref IEEE_TABLE: [[u32; 256]; 8] = make_table(IEEE);
+    pub static ref CASTAGNOLI_TABLE: [[u32; 256]; 8] = make_table(CASTAGNOLI);
+    pub static ref KOOPMAN_TABLE: [[u32; 256]; 8] = make_table(KOOPMAN);
 }
 
 pub struct Digest {
-    table: [u32; 256],
+    table: [[u32; 256]; 8],
     initial: u32,
     value: u32
 }
@@ -22,8 +23,8 @@ pub trait Hasher32 {
     fn sum32(&self) -> u32;
 }
 
-pub fn make_table(poly: u32) -> [u32; 256] {
-    let mut table = [0u32; 256];
+pub fn make_table(poly: u32) -> [[u32; 256]; 8] {
+    let mut table = [[0u32; 256]; 8];
     for i in 0..256 {
         let mut value = i as u32;
         for _ in 0..8 {
@@ -33,15 +34,42 @@ pub fn make_table(poly: u32) -> [u32; 256] {
                 value >> 1
             }
         }
-        table[i] = value;
+        table[0][i] = value;
+    }
+
+    for i in 0..256 {
+        table[1][i] = (table[0][i] >> 8) ^ table[0][(table[0][i] & 0xFF) as usize];
+        table[2][i] = (table[1][i] >> 8) ^ table[0][(table[1][i] & 0xFF) as usize];
+        table[3][i] = (table[2][i] >> 8) ^ table[0][(table[2][i] & 0xFF) as usize];
+        table[4][i] = (table[3][i] >> 8) ^ table[0][(table[3][i] & 0xFF) as usize];
+        table[5][i] = (table[4][i] >> 8) ^ table[0][(table[4][i] & 0xFF) as usize];
+        table[6][i] = (table[5][i] >> 8) ^ table[0][(table[5][i] & 0xFF) as usize];
+        table[7][i] = (table[6][i] >> 8) ^ table[0][(table[6][i] & 0xFF) as usize];
     }
     table
 }
 
-pub fn update(mut value: u32, table: &[u32; 256], bytes: &[u8]) -> u32 {
+pub fn update(mut value: u32, table: &[[u32; 256]; 8], bytes: &[u8]) -> u32 {
     value = !value;
-    for &i in bytes.iter() {
-        value = table[((value as u8) ^ i) as usize] ^ (value >> 8)
+    let mut i = 0;
+    while bytes.len() - i >= 8 {
+        let v1 = [bytes[i], bytes[i+1], bytes[i+2], bytes[i+3]];
+        let v2 = [bytes[i+4], bytes[i+5], bytes[i+6], bytes[i+7]];
+        let one = unsafe { mem::transmute::<[u8; 4], u32>(v1) } ^ value;
+        let two = unsafe { mem::transmute::<[u8; 4], u32>(v2) };
+        value =
+            table[0][(two >> 24) as usize] ^
+            table[1][((two >> 16) & 0xFF) as usize] ^
+            table[2][((two >> 8) & 0xFF) as usize] ^
+            table[3][(two & 0xFF) as usize] ^
+            table[4][(one >> 24) as usize] ^
+            table[5][((one >> 16) & 0xFF) as usize] ^
+            table[6][((one >> 8) & 0xFF) as usize] ^
+            table[7][(one & 0xFF) as usize];
+            i += 8;
+    }
+    for &e in bytes[i..].into_iter() {
+        value = table[0][((value as u8) ^ e) as usize] ^ (value >> 8)
     }
     !value
 }
