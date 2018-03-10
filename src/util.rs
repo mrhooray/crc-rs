813243dccb5904d2ca40a2366f1333ba41d0b790
janczer <menshikov.ivn@gmail.com>
2018-03-10 19:26:23 +0100

    Add crc16

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,19 @@
+pub fn make_table_crc16(poly: u16) -> [u16; 256] {
+    let mut table = [0u16; 256];
+    for i in 0..256 {
+        let mut value = i as u16;
+        for _ in 0..8 {
+            value = if (value & 1) == 1 {
+                (value >> 1) ^ poly
+            } else {
+                value >> 1
+            }
+        }
+        table[i] = value;
+    }
+    table
+}
+
 pub fn make_table_crc32(poly: u32) -> [u32; 256] {
     let mut table = [0u32; 256];
     for i in 0..256 {
