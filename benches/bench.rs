3576a01db9307dbff23bd2bdcf15207f968f0cc6
user <user@email.com>
2018-05-17 00:42:38 -0400

    Moved the benchmarks to the `criterion` crate

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,30 +1,30 @@
-#![feature(test)]
+#[macro_use]
+extern crate criterion;
 extern crate crc;
-extern crate test;
 
 use crc::{crc32, crc64};
-use test::Bencher;
+use criterion::{Criterion, Fun};
 
-#[bench]
-fn bench_crc32_make_table(b: &mut Bencher) {
-    b.iter(|| crc32::make_table(crc32::IEEE));
+/// Makes 4 benchmarks and runs them
+fn benchmarks(c: &mut Criterion) {
+    let bench_crc32 = Fun::new("CRC 32", |b, _i| b.iter(|| crc32::make_table(crc32::IEEE, true)));
+
+    let table = crc32::make_table(crc32::IEEE, true);
+    let bytes = Box::new([0u8; 100_000]);
+    let bench_crc32_mb = Fun::new("CRC 32 MBs", move |b, _i| b.iter(|| crc32::update(0, &table, &*bytes, true)));
+
+    let bench_crc64 = Fun::new("CRC 64", |b, _i|  b.iter(|| crc64::make_table(crc64::ECMA)));
+
+    let table2 = crc64::make_table(crc64::ECMA);
+    let bytes2 = Box::new([0u8; 100_000]);
+    let bench_crc64_mb = Fun::new("CRC 64 MBs", move |b, _i| b.iter(|| crc64::update(0, &table2, &*bytes2)));
+
+    // Build up a vector of the benchmark functions
+    let functions = vec!(bench_crc32, bench_crc32_mb, bench_crc64, bench_crc64_mb);
+
+    //Runs the benchmarks
+    c.bench_functions("Benchmarks", functions, &20);
 }
 
-#[bench]
-fn bench_crc32_update_megabytes(b: &mut Bencher) {
-    let table = crc32::make_table(crc32::IEEE);
-    let bytes = Box::new([0u8; 1_000_000]);
-    b.iter(|| crc32::update(0, &table, &*bytes));
-}
-
-#[bench]
-fn bench_crc64_make_table(b: &mut Bencher) {
-    b.iter(|| crc64::make_table(crc64::ECMA));
-}
-
-#[bench]
-fn bench_crc64_update_megabytes(b: &mut Bencher) {
-    let table = crc64::make_table(crc64::ECMA);
-    let bytes = Box::new([0u8; 1_000_000]);
-    b.iter(|| crc64::update(0, &table, &*bytes));
-}
+criterion_group!(benches, benchmarks);
+criterion_main!(benches);
