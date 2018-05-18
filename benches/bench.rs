08dc1153a2491eb2693a0f20aa4e57247e33424f
user <user@email.com>
2018-05-17 23:21:57 -0400

    Updated the MB benchmark to only do 991KB to prevent a stack overflow

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,20 +7,26 @@ use criterion::{Criterion, Fun};
 
 /// Makes 4 benchmarks and runs them
 fn benchmarks(c: &mut Criterion) {
-    let bench_crc32 = Fun::new("CRC 32", |b, _i| b.iter(|| crc32::make_table(crc32::IEEE, true)));
+    let bench_crc32 = Fun::new("CRC 32", |b, _i| {
+        b.iter(|| crc32::make_table(crc32::IEEE, true))
+    });
 
     let table = crc32::make_table(crc32::IEEE, true);
-    let bytes = Box::new([0u8; 100_000]);
-    let bench_crc32_mb = Fun::new("CRC 32 MBs", move |b, _i| b.iter(|| crc32::update(0, &table, &*bytes, true)));
+    let bytes = Box::new([0u8; 991_000]); // 1MB overflows the stack
+    let bench_crc32_mb = Fun::new("CRC 32 991kB", move |b, _i| {
+        b.iter(|| crc32::update(0, &table, &*bytes, true))
+    });
 
-    let bench_crc64 = Fun::new("CRC 64", |b, _i|  b.iter(|| crc64::make_table(crc64::ECMA)));
+    let bench_crc64 = Fun::new("CRC 64", |b, _i| b.iter(|| crc64::make_table(crc64::ECMA)));
 
     let table2 = crc64::make_table(crc64::ECMA);
-    let bytes2 = Box::new([0u8; 100_000]);
-    let bench_crc64_mb = Fun::new("CRC 64 MBs", move |b, _i| b.iter(|| crc64::update(0, &table2, &*bytes2)));
+    let bytes2 = Box::new([0u8; 991_000]); // 1MB overflows the stack
+    let bench_crc64_mb = Fun::new("CRC 64 001kB", move |b, _i| {
+        b.iter(|| crc64::update(0, &table2, &*bytes2))
+    });
 
     // Build up a vector of the benchmark functions
-    let functions = vec!(bench_crc32, bench_crc32_mb, bench_crc64, bench_crc64_mb);
+    let functions = vec![bench_crc32, bench_crc32_mb, bench_crc64, bench_crc64_mb];
 
     //Runs the benchmarks
     c.bench_functions("Benchmarks", functions, &20);
