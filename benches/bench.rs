3c5b9c193df59b9d5a4fdb59d859816dda8fb9a9
Moritz Borcherding <moritz.borcherding@web.de>
2023-01-21 00:33:49 +0100

    Add slice-by-16 implementation
    Introduce an implementation of the slice-by-16 algorithm that calculates the crc of 16 bytes in one step instead of iterating the input bytewise.

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,11 +4,17 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughpu
 pub const BLUETOOTH: Crc<u8> = Crc::<u8>::new(&CRC_8_BLUETOOTH);
 pub const X25: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_SDLC);
 pub const ISCSI: Crc<u32> = Crc::<u32>::new(&CRC_32_ISCSI);
+pub const ISCSI_SLICE16: Crc<Slice16<u32>> = Crc::<Slice16<u32>>::new(&CRC_32_ISCSI);
 pub const GSM_40: Crc<u64> = Crc::<u64>::new(&CRC_40_GSM);
 pub const ECMA: Crc<u64> = Crc::<u64>::new(&CRC_64_ECMA_182);
 pub const DARC: Crc<u128> = Crc::<u128>::new(&CRC_82_DARC);
 
 static KB: usize = 1024;
+
+fn baseline(data: &[u8]) -> usize {
+    data.iter()
+        .fold(0usize, |acc, v| acc.wrapping_add(*v as usize))
+}
 
 fn checksum(c: &mut Criterion) {
     let size = 16 * KB;
@@ -16,9 +22,13 @@ fn checksum(c: &mut Criterion) {
 
     c.benchmark_group("checksum")
         .throughput(Throughput::Bytes(size as u64))
+        .bench_function("baseline", |b| b.iter(|| baseline(black_box(&bytes))))
         .bench_function("crc8", |b| b.iter(|| BLUETOOTH.checksum(black_box(&bytes))))
         .bench_function("crc16", |b| b.iter(|| X25.checksum(black_box(&bytes))))
         .bench_function("crc32", |b| b.iter(|| ISCSI.checksum(black_box(&bytes))))
+        .bench_function("crc32_slice16", |b| {
+            b.iter(|| ISCSI_SLICE16.checksum(black_box(&bytes)))
+        })
         .bench_function("crc40", |b| b.iter(|| GSM_40.checksum(black_box(&bytes))))
         .bench_function("crc64", |b| b.iter(|| ECMA.checksum(black_box(&bytes))))
         .bench_function("crc82", |b| b.iter(|| DARC.checksum(black_box(&bytes))));
--- src/table.rs
+++ src/table.rs
@@ -44,8 +44,41 @@ pub(crate) const fn crc32_table(width: u8, poly: u32, reflect: bool) -> [u32; 25
 
     let mut table = [0u32; 256];
     let mut i = 0;
-    while i < table.len() {
+    while i < 256 {
         table[i] = crc32(poly, reflect, i as u32);
+        i += 1;
+    }
+
+    table
+}
+
+pub(crate) const fn crc32_table_slice_16(width: u8, poly: u32, reflect: bool) -> [[u32; 256]; 16] {
+    let poly = if reflect {
+        let poly = poly.reverse_bits();
+        poly >> (32u8 - width)
+    } else {
+        poly << (32u8 - width)
+    };
+
+    let mut table = [[0u32; 256]; 16];
+    let mut i = 0;
+    while i < 256 {
+        table[0][i] = crc32(poly, reflect, i as u32);
+        i += 1;
+    }
+
+    let mut i = 0;
+    while i < 256 {
+        let mut e = 1;
+        while e < 16 {
+            let one_lower = table[e - 1][i];
+            if reflect {
+                table[e][i] = (one_lower >> 8) ^ table[0][(one_lower & 0xFF) as usize];
+            } else {
+                table[e][i] = (one_lower << 8) ^ table[0][((one_lower >> 24) & 0xFF) as usize];
+            }
+            e += 1;
+        }
         i += 1;
     }
     table
