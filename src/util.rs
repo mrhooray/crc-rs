ab7ab7d20f427e3716f71a09e2349da6aa7345c8
akhilles <akhilvelagapudi@gmail.com>
2025-05-06 21:15:39 -0700

    Revert "Add cargo features for each CRC width"
    This reverts commit 66cb36e8ed87f85687eab1396bbeaefdb423d80d.

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,3 @@
-#[cfg(feature = "crc8")]
 pub(crate) const fn crc8(poly: u8, reflect: bool, mut value: u8) -> u8 {
     let mut i = 0;
     if reflect {
@@ -15,7 +14,6 @@ pub(crate) const fn crc8(poly: u8, reflect: bool, mut value: u8) -> u8 {
     value
 }
 
-#[cfg(feature = "crc16")]
 pub(crate) const fn crc16(poly: u16, reflect: bool, mut value: u16) -> u16 {
     if reflect {
         let mut i = 0;
@@ -35,7 +33,6 @@ pub(crate) const fn crc16(poly: u16, reflect: bool, mut value: u16) -> u16 {
     value
 }
 
-#[cfg(feature = "crc32")]
 pub(crate) const fn crc32(poly: u32, reflect: bool, mut value: u32) -> u32 {
     if reflect {
         let mut i = 0;
@@ -55,7 +52,6 @@ pub(crate) const fn crc32(poly: u32, reflect: bool, mut value: u32) -> u32 {
     value
 }
 
-#[cfg(feature = "crc64")]
 pub(crate) const fn crc64(poly: u64, reflect: bool, mut value: u64) -> u64 {
     if reflect {
         let mut i = 0;
@@ -75,7 +71,6 @@ pub(crate) const fn crc64(poly: u64, reflect: bool, mut value: u64) -> u64 {
     value
 }
 
-#[cfg(feature = "crc128")]
 pub(crate) const fn crc128(poly: u128, reflect: bool, mut value: u128) -> u128 {
     if reflect {
         let mut i = 0;
